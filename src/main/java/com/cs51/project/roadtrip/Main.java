777b1cf3968ba45f190e629c4fcfa97dfcd85ce2
Robert <rschupp@g.harvard.edu>
2015-04-16 09:35:03 -0500

    RoadTrip - added a main class to project that creates the UI and executes it... added a couple new methods to the UI interface

---
+++ src/main/java/com/cs51/project/roadtrip/Main.java
@@ -0,0 +1,14 @@
+package com.cs51.project.roadtrip;
+
+/**
+ * Created by robertschupp on 4/16/15.
+ */
+public class Main {
+
+    public static void main (String [] args) {
+        UI commandUI = new CommandLineUI();
+        commandUI.initialize();
+        commandUI.execute();
+    }
+
+}

432b6400faf1f6cac86c9b1d173a9730086b1b60
Alexander <alexander.schroeders@googlemail.com>
2015-04-21 18:46:56 +0200

    Updated BasicComparisonService with some functionality

--- src/main/java/com/cs51/project/roadtrip/services/BasicComparisonService.java
+++ src/main/java/com/cs51/project/roadtrip/services/BasicComparisonService.java
@@ -8,6 +8,7 @@ import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Created by robertschupp on 4/16/15.
@@ -19,12 +20,16 @@ public class BasicComparisonService implements IComparisonService {
      */
     private static Logger logger = Logger.getLogger(BasicComparisonService.class);
 
+
     public List<Result> executeComparison(IGraph graph, List<IAlgorithm> algs) {
 
-        //TODO i just stubbed this out for the command UI while I was working on it.. feel free to do as you please
-        Result result = new Result();
+        //create a list for our results
         List<Result> results = new ArrayList<>();
-        results.add(result);
+
+        //execute each algorithm and store the result
+        Stream<IAlgorithm> algStream = algs.stream();
+        algStream.forEach(alg -> results.add(alg.execute(graph)));
+        algStream.close();
 
         return results;
     }

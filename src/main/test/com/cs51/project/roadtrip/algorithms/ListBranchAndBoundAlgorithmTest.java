3eb4d96e75ce94ab524b63482626988b828d3d69
schroeax <alexander.schroeders@googlemail.com>
2015-04-27 15:32:19 +0200

    Basic version of Tests for all Algorithms

--- src/main/test/com/cs51/project/roadtrip/algorithms/ListBranchAndBoundAlgorithmTest.java
+++ src/main/test/com/cs51/project/roadtrip/algorithms/ListBranchAndBoundAlgorithmTest.java
@@ -1,13 +1,19 @@
 package com.cs51.project.roadtrip.algorithms;
 
+import com.cs51.project.roadtrip.common.dto.Result;
 import com.cs51.project.roadtrip.graphs.ListGraph;
 import com.cs51.project.roadtrip.graphs.Node;
+import com.cs51.project.roadtrip.interfaces.IAlgorithm;
 import com.cs51.project.roadtrip.interfaces.IGraph;
 import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Test class for the branch and bound algorithm implementation
@@ -20,97 +26,84 @@ public class ListBranchAndBoundAlgorithmTest {
     private static Logger logger = Logger.getLogger(ListBranchAndBoundAlgorithmTest.class);
 
 
-    @Test
-    public void testTest() {
+    private static IGraph graph;
+    private static IGraph graph2;
 
-        Node n0 = new Node(0, 25, 13, "N0", true);
-        Node n1 = new Node(1, 50, 89, "N1", false);
-        Node n2 = new Node(2, 99, 77, "N2", false);
-        Node n3 = new Node(3, 43, 4, "N3", false);
-        List<Node> baseList = new ArrayList<>();
+    private static List<Node> baseList;
+    private static List<Node> baseList2;
+
+    private static Node n0;
+    private static Node n1;
+    private static Node n2;
+    private static Node n3;
+
+    private static Node n20;
+    private static Node n21;
+    private static Node n22;
+    private static Node n23;
+
+    @BeforeClass
+    public static void initTest(){
+        n0 = new Node(0, 99, 13, "N0", true);
+        n1 = new Node(1, 50, 89, "N1", false);
+        n2 = new Node(2, 99, 77, "N2", false);
+        n3 = new Node(3, 43, 4, "N3", false);
+        baseList = new ArrayList<>(4);
         baseList.add(n0);
         baseList.add(n1);
         baseList.add(n2);
         baseList.add(n3);
 
-//        IGraph graph = new ListGraph(baseList);
-//        IGraph graph = new ListGraph(13);
-//
-//        BranchAndBoundAlgorithm alg = new BranchAndBoundAlgorithm();
+        graph = new ListGraph(baseList);
 
+        n20 = new Node(0, 0, 100, "N0", true);
+        n21 = new Node(2, 0, 0, "N2", false);
+        n22 = new Node(1, 100, 0, "N1", false);
+        n23 = new Node(3, 100, 100, "N3", false);
+        baseList2 = new ArrayList<>(4);
+        baseList2.add(n20);
+        baseList2.add(n21);
+        baseList2.add(n22);
+        baseList2.add(n23);
 
-//        List<Node> l1 = new ArrayList<>();
-//        l1.add(n0);
-//        l1.add(n1);
-//        l1.add(n2);
-//        l1.add(n3);
-//        l1.add(n0);
-//
-//        List<Node> l2 = new ArrayList<>();
-//        l2.add(n0);
-//        l2.add(n1);
-//        l2.add(n3);
-//        l2.add(n2);
-//        l2.add(n0);
-//
-//        List<Node> l3 = new ArrayList<>();
-//        l3.add(n0);
-//        l3.add(n2);
-//        l3.add(n1);
-//        l3.add(n3);
-//        l3.add(n0);
-//
-//        List<Node> l4 = new ArrayList<>();
-//        l4.add(n0);
-//        l4.add(n2);
-//        l4.add(n3);
-//        l4.add(n1);
-//        l4.add(n0);
-//
-//        List<Node> l5 = new ArrayList<>();
-//        l5.add(n0);
-//        l5.add(n3);
-//        l5.add(n1);
-//        l5.add(n2);
-//        l5.add(n0);
-//
-//        List<Node> l6 = new ArrayList<>();
-//        l6.add(n0);
-//        l6.add(n3);
-//        l6.add(n2);
-//        l6.add(n1);
-//        l6.add(n0);
-//
-//        List<List<Node>> possibilities = new ArrayList<>();
-//        possibilities.add(l1);
-//        possibilities.add(l2);
-//        possibilities.add(l3);
-//        possibilities.add(l4);
-//        possibilities.add(l5);
-//        possibilities.add(l6);
-//
-//        for (List<Node> l : possibilities) {
-//            StringBuilder sb = new StringBuilder();
-//            for (Node n : l) {
-//                sb.append(n.getName() + " -> ");
-//            }
-//
-//            sb.append(alg.getD(l, graph));
-//            System.out.println(sb.toString());
-//        }
+        graph2 = new ListGraph(baseList2);
 
-for (int i = 0; i < 5; i++) {
-    IGraph graph = new ListGraph(7);
-
-    ListBranchAndBoundAlgorithm alg = new ListBranchAndBoundAlgorithm();
-
-//        IGraph graph = new ListGraph(3);
-    Long startTime = System.currentTimeMillis();
-    alg.execute(graph);
-    System.out.println("exe time = " + (System.currentTimeMillis() - startTime));
-}
     }
 
 
+    @Test
+    public void executeTest (){
+        IAlgorithm alg = new ListBranchAndBoundAlgorithm();
+
+        Result result = alg.execute(graph);
+        Result result2 = alg.execute(graph2);
+
+        //fill expected result
+        List<Object> expectedResult = new ArrayList<>(4);
+        expectedResult.add(n0);
+        expectedResult.add(n3);
+        expectedResult.add(n1);
+        expectedResult.add(n2);
+        expectedResult.add(n0);
+
+        List<Object> expectedResult2 = new ArrayList<>(4);
+        expectedResult2.add(n20);
+        expectedResult2.add(n21);
+        expectedResult2.add(n22);
+        expectedResult2.add(n23);
+        expectedResult2.add(n20);
+
+        //check the result values of the TreeBranchAndBound
+
+        assertEquals(26, result.getIterations());
+        assertEquals(expectedResult, result.getCalculatedPath());
+        assertEquals(new BigDecimal("256.46"), result.getCalculatedDistance());
+        assertEquals(4, result.getGraphSize());
+
+        assertEquals(27, result2.getIterations());
+        assertEquals(expectedResult2, result2.getCalculatedPath());
+        assertEquals(new BigDecimal("400.00"), result2.getCalculatedDistance());
+        assertEquals(4,result2.getGraphSize());
+    }
 
 }
--- src/main/test/com/cs51/project/roadtrip/algorithms/NearestNeighborAlgorithmTest.java
+++ src/main/test/com/cs51/project/roadtrip/algorithms/NearestNeighborAlgorithmTest.java
@@ -1,6 +1,19 @@
 package com.cs51.project.roadtrip.algorithms;
 
+import com.cs51.project.roadtrip.common.dto.Result;
+import com.cs51.project.roadtrip.graphs.ListGraph;
+import com.cs51.project.roadtrip.graphs.Node;
+import com.cs51.project.roadtrip.interfaces.IAlgorithm;
+import com.cs51.project.roadtrip.interfaces.IGraph;
 import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Test class for the NearestNeighbor algorithm implementation
@@ -11,4 +24,84 @@ public class NearestNeighborAlgorithmTest {
      * Logger Instance
      */
     private static Logger logger = Logger.getLogger(NearestNeighborAlgorithmTest.class);
+
+    private static IGraph graph;
+    private static IGraph graph2;
+
+    private static List<Node> baseList;
+    private static List<Node> baseList2;
+
+    private static Node n0;
+    private static Node n1;
+    private static Node n2;
+    private static Node n3;
+
+    private static Node n20;
+    private static Node n21;
+    private static Node n22;
+    private static Node n23;
+
+    @BeforeClass
+    public static void initTest(){
+        n0 = new Node(0, 99, 13, "N0", true);
+        n1 = new Node(1, 50, 89, "N1", false);
+        n2 = new Node(2, 99, 77, "N2", false);
+        n3 = new Node(3, 43, 4, "N3", false);
+        baseList = new ArrayList<>(4);
+        baseList.add(n0);
+        baseList.add(n1);
+        baseList.add(n2);
+        baseList.add(n3);
+
+        graph = new ListGraph(baseList);
+
+        n20 = new Node(0, 0, 100, "N0", true);
+        n21 = new Node(2, 0, 0, "N2", false);
+        n22 = new Node(1, 100, 0, "N1", false);
+        n23 = new Node(3, 100, 100, "N3", false);
+        baseList2 = new ArrayList<>(4);
+        baseList2.add(n20);
+        baseList2.add(n21);
+        baseList2.add(n22);
+        baseList2.add(n23);
+
+        graph2 = new ListGraph(baseList2);
+
+    }
+
+
+    @Test
+    public void executeTest (){
+        IAlgorithm alg = new NearestNeighborAlgorithm();
+
+        Result result = alg.execute(graph);
+        Result result2 = alg.execute(graph2);
+
+        //fill expected result
+        List<Object> expectedResult = new ArrayList<>(4);
+        expectedResult.add(n0);
+        expectedResult.add(n3);
+        expectedResult.add(n1);
+        expectedResult.add(n2);
+        expectedResult.add(n0);
+
+        List<Object> expectedResult2 = new ArrayList<>(4);
+        expectedResult2.add(n20);
+        expectedResult2.add(n21);
+        expectedResult2.add(n22);
+        expectedResult2.add(n23);
+        expectedResult2.add(n20);
+
+        //check the result values of the NearestNeighbor
+
+        assertEquals(0, result.getIterations());
+        assertEquals(expectedResult, result.getCalculatedPath());
+        assertEquals(new BigDecimal("256.46"), result.getCalculatedDistance());
+        assertEquals(4, result.getGraphSize());
+
+        assertEquals(0, result2.getIterations());
+        assertEquals(expectedResult2, result2.getCalculatedPath());
+        assertEquals(new BigDecimal("400.00"), result2.getCalculatedDistance());
+        assertEquals(4,result2.getGraphSize());
+    }
 }
